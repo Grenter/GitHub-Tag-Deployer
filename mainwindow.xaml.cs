77285e842da595974b027a176cced2cf4b87b9ef
David Grenter <davidgrenter@gmail.com>
2013-01-30 21:34:33 +0000

    Changes
    Turned Styles off Temporally.
    Made Changes to UI ready for backing up to a seperate directory

--- mainwindow.xaml.cs
+++ mainwindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -81,6 +82,11 @@ namespace GitHub_Tag_Deployer
 
             txt.Foreground = Brushes.Black;
             txt.Text = "";
+        }
+
+        private void btnBackUpBrowse_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
